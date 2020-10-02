95403399884655ebf5116ec5243d2060179af670
dibrinsofor <64705019+dibrinsofor@users.noreply.github.com>
2020-10-01 19:32:56 -0500

    Update settings.py

--- try2/settings.py
+++ try2/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
