378a51a0fde1f9892a2b37c4aa8586265f6a8509
dibrinsofor <64705019+dibrinsofor@users.noreply.github.com>
2020-10-02 07:51:38 -0500

    Update views.py

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import LocationSerializer
 from .models import Locations
-from .forms import ImageForm
+# from .forms import ImageForm
 
 # Create your views here.
 @api_view(['GET', 'POST', 'DELETE'])
