c882e0688ff5aa1179ece98355d1386ba3c1fc78
dibrinsofor <64705019+dibrinsofor@users.noreply.github.com>
2020-10-02 07:40:49 -0500

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-def user_directory_path(instance, filename): 
+# def user_directory_path(instance, filename): 
   
-    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
-    return 'user_{0}/{1}'.format(instance.user.id, filename) 
+#     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
+#     return 'user_{0}/{1}'.format(instance.user.id, filename) 
 # def upload_location(instance, filename, **kwargs):
 #     file_path = '{author_id/}/{title}-{filename}'.format(
 #         author_id = str(instance.author.id), title=str(instance.title), filename=filename
@@ -13,12 +13,17 @@ def user_directory_path(instance, filename):
 
 # Create your models here.
 class Locations(models.Model):
-    Name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
     location = models.CharField(max_length=20)
     locationDescription = models.CharField(max_length=140)
     country = models.CharField(max_length=20)
 #     image = models.ImageField(upload_to=upload_location, null=False, blank=False)
-    uploadImage = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+#     name= models.CharField(max_length=500)
+    videofile= models.FileField(upload_to='images/', null=True, verbose_name="")
+
+    def __str__(self):
+        return self.name + ": " + str(self.imagefile)
+
 
     ###dont firget to add url path for image now
 
