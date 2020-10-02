d19a15a0a478ea93d5181da1b175d079c532db61
dibrinsofor <64705019+dibrinsofor@users.noreply.github.com>
2020-10-02 07:53:58 -0500

    Update models.py

--- products/models.py
+++ products/models.py
@@ -4,11 +4,11 @@ from django.db import models
   
 #     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename> 
 #     return 'user_{0}/{1}'.format(instance.user.id, filename) 
-# def upload_location(instance, filename, **kwargs):
-#     file_path = '{author_id/}/{title}-{filename}'.format(
-#         author_id = str(instance.author.id), title=str(instance.title), filename=filename
-#     )
-#     return file_path    
+def upload_location(instance, filename, **kwargs):
+    file_path = '{filename}'.format(
+        filename=filename
+    )
+    return file_path    
 
 
 # Create your models here.
@@ -17,13 +17,7 @@ class Locations(models.Model):
     location = models.CharField(max_length=20)
     locationDescription = models.CharField(max_length=140)
     country = models.CharField(max_length=20)
-#     image = models.ImageField(upload_to=upload_location, null=False, blank=False)
-#     name= models.CharField(max_length=500)
-    videofile= models.FileField(upload_to='images/', null=True, verbose_name="")
-
-    def __str__(self):
-        return self.name + ": " + str(self.imagefile)
-
+    image = models.ImageField(upload_to=upload_location, null=False, blank=False)
 
     ###dont firget to add url path for image now
 
