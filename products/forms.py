38b324187784a12f47df59e0fb16716d9f5dc80e
dibrinsofor <64705019+dibrinsofor@users.noreply.github.com>
2020-10-02 07:48:01 -0500

    Delete forms.py

--- products/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model= Location
-        fields= ["name", "imagefile", "country", "location", "locationDescription"]
